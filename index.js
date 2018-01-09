b94e472bbddaff118513b6637aa9dab6e9a19729
Szewah Chin <swgrleong@gmail.com>
2018-01-09 01:02:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,28 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() *100) + 1
+  var itemObject = {}
+  itemObject[item] = price
+  cart.push(itemObject)
+  console.log(`$(item) has been added to your cart.`)
+  return cart
+
 }
 
 function viewCart() {
-  // write your code here
+  if (cart === 0) {
+    console.log('Your shopping cart is empty')
+  } else {
+    var items = []
+    for (var i = 0; i < cart.length; i++) {
+      for (var item in cart[i])
+      items.push(item + " at $" + cart[i][item])
+    }
+  }
+    console.log("In your cart, you have " + items.join(", ") + ".")
 }
+
 
 function total() {
   // write your code here
